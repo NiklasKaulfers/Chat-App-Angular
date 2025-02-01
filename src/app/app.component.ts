b0855a0d2f804d28318b3549a78d7fc830c5c87f
NiklasKaulfers <niklas.kaulfers@zeiss.com>
2025-02-01 16:08:02 +0100

    added tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './components/header/header.component';
+import {StartPageComponent} from './components/start-page/start-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, StartPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'chat-app-ng-frontend';
+
+  loggedIn:boolean = false;
+
 }
