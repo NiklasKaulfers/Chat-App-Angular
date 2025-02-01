f6db5fbaf384d434a6b708677f5e5811c14437e0
NiklasKaulfers <niklas.kaulfers@zeiss.com>
2025-02-01 16:33:49 +0100

    removed tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from './components/header/header.component';
 import {StartPageComponent} from './components/start-page/start-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, StartPageComponent],
+  imports: [RouterOutlet, StartPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
