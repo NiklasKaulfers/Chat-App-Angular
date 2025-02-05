e10b4c18425438a8bdb56c016221ddf3ad62d66d
Niklas Kaulfers <niklas.kaulfers2@gmail.com>
2025-02-05 17:44:34 +0100

    heeader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {StartPageComponent} from './components/start-page/start-page.component';
+import {HeaderComponent} from './components/header/header.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StartPageComponent],
+  imports: [RouterOutlet, StartPageComponent, NgIf, RouterLink, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'chat-app-ng-frontend';
-
-  loggedIn:boolean = false;
-
 }
--- src/app/components/start-page/start-page.component.ts
+++ src/app/components/start-page/start-page.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-start-page',
   templateUrl: './start-page.component.html',
+  imports: [
+    NgIf
+  ],
   styleUrls: ['./start-page.component.css']
 })
 export class StartPageComponent {
