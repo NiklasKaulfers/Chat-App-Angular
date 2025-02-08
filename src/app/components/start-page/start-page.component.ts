3c96a668dd94a33dbba0c15282500b19c86a5e13
Niklas Kaulfers <niklas.kaulfers2@gmail.com>
2025-02-08 02:36:53 +0100

    fixes for navbar 2

--- src/app/components/start-page/start-page.component.ts
+++ src/app/components/start-page/start-page.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import {Router, RouterLink} from '@angular/router';
+import {RouterLink} from '@angular/router';
 import {NgIf} from '@angular/common';
-import {LoginComponent} from '../login/login.component';
 
 @Component({
   selector: 'app-start-page',
