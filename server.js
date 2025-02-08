7d3d0db5b4e3dfafb45f0fdbfe828aa33061889c
Niklas Kaulfers <niklas.kaulfers2@gmail.com>
2025-02-08 02:10:22 +0100

    with heroku jajjjj

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist', 'chat-app-angular')));
+const distPath = path.join(__dirname, "dist/chat-app-ng-frontend");
+app.use(express.static(distPath));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'chat-app-angular', 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
