f3b3c9c50c2b210b328613a841e07fca3775a02c
Niklas Kaulfers <niklas.kaulfers2@gmail.com>
2025-02-08 01:46:28 +0100

    navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    NgIf
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  username: string = '';
+
+  ngOnInit() {
+    this.checkLoginStatus();
+  }
+
+  checkLoginStatus() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.isLoggedIn = true;
+      this.username = localStorage.getItem('username') || 'User';
+      this.isLoggedIn = false;
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    this.isLoggedIn = false;
+    this.username = '';
+    this.router.navigate(['/start']);
+  }
+  constructor(private router: Router) {}
+}
