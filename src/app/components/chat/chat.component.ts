d65899d60662478e47cf44426b608b5b2a98086b
Niklas Kaulfers <niklas.kaulfers2@gmail.com>
2025-02-17 18:59:40 +0100

    fixes according to aws docs

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -15,6 +15,7 @@ export class ChatComponent implements OnInit {
   userInput: string = '';
   messages: { sender: string; message: string }[] = [];
   channel: any;
+  room: string | null = localStorage.getItem('room');
 
   ngOnInit(): void {
     this.initializeChat();
@@ -25,9 +26,9 @@ export class ChatComponent implements OnInit {
       Amplify.configure({
         "API": {
           "Events": {
-            endpoint: "https://jleaiewm4jdsrd2wm2coiwuwnu.appsync-api.eu-central-1.amazonaws.com/event",
-            region: "eu-central-1",
-            defaultAuthMode: "lambda"
+            "endpoint": "https://jleaiewm4jdsrd2wm2coiwuwnu.appsync-api.eu-central-1.amazonaws.com/event",
+            "region": "eu-central-1",
+            "defaultAuthMode": "lambda"
           }
         }
       });
@@ -36,9 +37,10 @@ export class ChatComponent implements OnInit {
       this.channel = await events.connect(`/default/${room}`);
 
       this.channel.subscribe({
-        error: (error: any) => console.error("Error receiving data:", error),
+        error: (error: any) => {
+          this.displayMessage("Server", "Subscription failed.");
+        },
         next: (data: any) => {
-          console.log("Received data:", data);
           this.displayMessage(data.sender, data.message);
         }
       });
@@ -50,16 +52,25 @@ export class ChatComponent implements OnInit {
   async submitText() {
     if (!this.userInput.trim()) return;
 
+    const sender: string | null= localStorage.getItem("user");
+
+    if (!sender) {
+      this.displayMessage("Server", "Please log in.")
+      return;
+    }
+
     const messageData = {
-      sender: "User", // Replace with actual username logic
+      sender: sender,
       message: this.userInput
     };
 
     this.messages.push(messageData);
     this.userInput = '';
-
+    if (!this.room){
+      return;
+    }
     try {
-      await this.channel.publish(messageData);
+      await this.channel.post(`/default/${this.room}`,[messageData]);
     } catch (error) {
       console.error("Error sending message:", error);
     }
