bff05b6f14807eaf4c2ad8de1c539796f9bd68af
Niklas Kaulfers <niklas.kaulfers2@gmail.com>
2025-02-17 16:29:16 +0100

    added room creation and fixed routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,13 @@ import {StartPageComponent} from './components/start-page/start-page.component';
 import {RegisterComponent} from './components/register/register.component';
 import {RoomListComponent} from './components/room-list/room-list.component';
 import {ChatComponent} from './components/chat/chat.component';
+import {CreateRoomComponent} from './components/create-room/create-room.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'rooms', component: RoomListComponent },
+  { path: 'joinRoom', component: RoomListComponent },
+  { path: 'rooms', redirectTo: '/joinRoom', pathMatch: 'full' },
+  { path: 'createRoom', component: CreateRoomComponent },
   { path: 'start', component: StartPageComponent },
   { path: 'chat', component: ChatComponent },
   { path: '', redirectTo: "start", pathMatch: 'full' },
--- src/app/components/create-room/create-room.component.ts
+++ src/app/components/create-room/create-room.component.ts
@@ -1,11 +1,68 @@
 import { Component } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-create-room',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgClass
+  ],
   templateUrl: './create-room.component.html',
   styleUrl: './create-room.component.css'
 })
 export class CreateRoomComponent {
+  creationFeedback: string = "";
+  buttonPressed = false;
+  title: string = "";
+  passwordFirst: string = "";
+  passwordSecond: string = "";
+  showPassword: boolean = false;
 
+  togglePasswordVisibility = () => {
+    this.showPassword = !this.showPassword;
+  };
+
+  createRoom = async (): Promise<void> => {
+    this.buttonPressed = true;
+
+    if (!this.title) {
+      this.creationFeedback = "Needs a title";
+      return;
+    }
+
+    let pin = "";
+    if ((this.passwordFirst || this.passwordSecond) && this.passwordFirst === this.passwordSecond) {
+      pin = this.passwordFirst;
+    } else {
+      this.creationFeedback = "Passwords don't match";
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        "https://web-ing-iib23-chat-app-backend-377dbfe5320c.herokuapp.com/api/rooms",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            pin: pin,
+            display_name: `${this.title}`,
+          }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (data.success) {
+        this.creationFeedback = `created room: ${this.title}`;
+      } else {
+        this.creationFeedback = "Server error occurred";
+      }
+    } catch (error) {
+      this.creationFeedback = "Network error occurred";
+      console.error("Error:", error);
+    }
+  };
 }
