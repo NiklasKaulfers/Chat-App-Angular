8de57843864b43c202acb6031732150d8cf328c5
Niklas Kaulfers <niklas.kaulfers2@gmail.com>
2025-02-19 18:39:59 +0100

    fix for reloading

--- src/app/components/start-page/start-page.component.ts
+++ src/app/components/start-page/start-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgIf } from '@angular/common';
-import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-start-page',
@@ -13,37 +12,18 @@ import { NavbarComponent } from '../navbar/navbar.component';
   ],
   styleUrls: ['./start-page.component.css']
 })
-export class StartPageComponent implements OnInit, OnDestroy {
-  loggedIn: boolean = false;
-  private authCheckInterval: any; // Interval reference
+export class StartPageComponent {
+  loggedIn: boolean = this.checkAuthState();
 
   logout() {
     localStorage.clear();
     window.location.reload();
   }
 
-  checkAuthState() {
+  checkAuthState(): boolean {
     const token = localStorage.getItem('token');
-    const isLoggedIn = !!token;
-
-    if (this.loggedIn !== isLoggedIn) {
-      this.loggedIn = isLoggedIn;
-      window.location.reload();
-    }
+    return !!token;
   }
 
-  ngOnInit(): void {
-    this.checkAuthState();
 
-    // Check for changes every 2 seconds
-    this.authCheckInterval = setInterval(() => {
-      this.checkAuthState();
-    }, 2000);
-  }
-
-  ngOnDestroy(): void {
-    if (this.authCheckInterval) {
-      clearInterval(this.authCheckInterval);
-    }
-  }
 }
